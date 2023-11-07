3b4af1c0797ed92c02c25bc464eeabd125d84be2
Roman <barabanov1233@gmail.com>
2023-11-07 15:08:30 +0400

    Send logs before signTransaction

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import { AccountInterface, Invocation, InvocationsDetailsWithNonce } from 'starknet'
+import { Abi, AccountInterface, Call, Invocation, InvocationsDetailsWithNonce, InvocationsSignerDetails } from 'starknet'
+import { transaction } from 'starknet'
 
 interface WalnutTransactionLog {
-	chain_id: number
-	contract_address: string
+	chain_id: string
+	wallet_address: string
 	calldata: Invocation['calldata']
 	nonce: number
-	max_fee?: number
-	version?: number
+	max_fee: number
+	version: number
+	cairo_version: string
 }
 
 async function sendLog(log: WalnutTransactionLog, apiKey: string) {
-	const url = 'http://127.0.0.1:3000/simulate'
+	const url = 'https://api.walnut.dev/simulate'
 	try {
 		fetch(url, {
 			method: 'POST',
@@ -20,18 +22,19 @@ async function sendLog(log: WalnutTransactionLog, apiKey: string) {
 	} catch {}
 }
 
-export function addWalnutLogs({ account, apiKey, chainId }: { account: AccountInterface; apiKey: string; chainId: number }) {
-	const originalMethod = account.invokeFunction
-	account.invokeFunction = async function (...args: [functionInvocation: Invocation, details: InvocationsDetailsWithNonce]) {
-		const functionInvocation = args[0]
-		const details = args[1]
+export function addWalnutLogs({ account, apiKey }: { account: AccountInterface; apiKey: string }) {
+	const originalMethod = account.signer.signTransaction
+	account.signer.signTransaction = async function (...args: [transactions: Call[], transactionsDetail: InvocationsSignerDetails, abis?: Abi[]]) {
+		const transactions = args[0]
+		const transactionsDetail = args[1]
 		const log: WalnutTransactionLog = {
-			chain_id: chainId,
-			contract_address: functionInvocation.contractAddress,
-			calldata: functionInvocation.calldata,
-			nonce: Number(details.nonce),
-			max_fee: Number(details.maxFee),
-			version: Number(details.version),
+			chain_id: transactionsDetail.chainId,
+			wallet_address: transactionsDetail.walletAddress,
+			calldata: transaction.getExecuteCalldata(transactions, transactionsDetail.cairoVersion),
+			nonce: Number(transactionsDetail.nonce),
+			max_fee: Number(transactionsDetail.maxFee),
+			version: Number(transactionsDetail.version),
+			cairo_version: transactionsDetail.cairoVersion,
 		}
 		sendLog(log, apiKey)
 		return originalMethod.apply(this, args)
