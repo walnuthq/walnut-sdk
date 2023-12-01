207edf0f32f743cd9201b261f8fcf528dc6a0444
Roman <barabanov1233@gmail.com>
2023-12-01 19:03:24 +0400

    Fetch wallet cairo version

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { AllowArray, CairoVersion, Call, Invocation, InvokeFunctionResponse, transaction } from 'starknet'
+import { AllowArray, CairoVersion, Call, Invocation, InvokeFunctionResponse, RpcProvider, transaction } from 'starknet'
 
 interface WalnutTransactionLog {
 	chain_id: string
@@ -25,7 +25,9 @@ async function sendLog(apiKey: string, account: RequiredAccountMethods, calls: A
 	try {
 		const transactions = Array.isArray(calls) ? calls : [calls]
 		const chainId = await account.getChainId()
-		const cairo_version: CairoVersion | undefined = account.cairoVersion ?? (account.getCairoVersion ? await account.getCairoVersion() : undefined)
+		// const cairo_version: CairoVersion | undefined = account.cairoVersion ?? (account.getCairoVersion ? await account.getCairoVersion() : undefined)
+		const provider = new RpcProvider({ nodeUrl: 'https://ikah.goerli1-juno.rpc.nethermind.io' })
+		const { cairo: cairo_version } = await provider.getContractVersion(account.address)
 		const calldata = transaction.getExecuteCalldata(transactions, cairo_version)
 		const max_fee = transactionsDetail?.maxFee ? Number(transactionsDetail?.maxFee) : undefined
 		const log: WalnutTransactionLog = {
